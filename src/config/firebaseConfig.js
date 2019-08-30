859e20955c3447ff3cadf7b3ea8409d8b41f0043
Kat Grennan <caitlyn.grennan@techtonic.com>
2019-08-30 16:18:03 -0600

    Add firebase config

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBt72XyYFulIDATXdJsfEPQsHIRWlW8uA8",
+  authDomain: "capstone-ccb786.firebaseapp.com",
+  databaseURL: "https://capstone-ccb786.firebaseio.com",
+  projectId: "capstone-ccb786",
+  storageBucket: "capstone-ccb786.appspot.com",
+  messagingSenderId: "835017476260",
+  appId: "1:835017476260:web:e32d33efca273ae1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
